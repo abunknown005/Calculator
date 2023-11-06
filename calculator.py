f4af1e3ba8f8b987ef992d3c6ad9a46b2f054037
abunknown005 <150009177+abunknown005@users.noreply.github.com>
2023-11-06 15:16:26 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,7 @@
+def calculator(n1,n2):
+    sum1=n1+n2
+    return sum1
+num1=float(input("Enter the first number:"))
+num2=float(input("Enter the second number:"))
+s=calculator(num1,num2)
+print("Sum=",s)
